ba9a617eed554423fd0e6f32c18b4ae4c702072d
Edson Shideki Kokado <eskokado@gmail.com>
2019-06-24 15:21:33 -0300

    Consultando primeiro recurso com GET

---
+++ src/main/java/br/com/eskinfotechweb/algamoneyapi/resource/CategoriaResource.java
@@ -0,0 +1,24 @@
+package br.com.eskinfotechweb.algamoneyapi.resource;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.eskinfotechweb.algamoneyapi.model.Categoria;
+import br.com.eskinfotechweb.algamoneyapi.repository.CategoriaRepository;
+
+@RestController
+@RequestMapping("/categorias")
+public class CategoriaResource {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
+	@GetMapping
+	public List<Categoria> listar() {
+		return categoriaRepository.findAll();
+	}
+}
