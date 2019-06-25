dd2bc4dbde219d0766e484e09da92daff2f5050c
Edson Shideki Kokado <eskokado@gmail.com>
2019-06-25 10:18:08 -0300

    Atualizando pessoa com PUT

--- src/main/java/br/com/eskinfotechweb/algamoneyapi/resource/PessoaResource.java
+++ src/main/java/br/com/eskinfotechweb/algamoneyapi/resource/PessoaResource.java
@@ -1,6 +1,5 @@
 package br.com.eskinfotechweb.algamoneyapi.resource;
 
-import java.net.URI;
 import java.util.List;
 
 import javax.servlet.http.HttpServletResponse;
@@ -14,20 +13,24 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.com.eskinfotechweb.algamoneyapi.event.RecursoCriadoEvent;
 import br.com.eskinfotechweb.algamoneyapi.model.Pessoa;
 import br.com.eskinfotechweb.algamoneyapi.repository.PessoaRepository;
+import br.com.eskinfotechweb.algamoneyapi.service.PessoaService;
 
 @RestController
 @RequestMapping("/pessoas")
 public class PessoaResource {
 
+	@Autowired
+	private PessoaService pessoaService;
+	
 	@Autowired
 	private PessoaRepository pessoaRepository;
 	
@@ -60,4 +63,11 @@ public class PessoaResource {
 	public void remove(@PathVariable Long codigo) {
 		pessoaRepository.delete(codigo);
 	}
+	
+	@PutMapping("/{codigo}")
+	public ResponseEntity<Pessoa> atualizar(@PathVariable Long codigo, @Valid @RequestBody Pessoa pessoa) {
+		Pessoa pessoaSalva = pessoaService.Atualizar(codigo, pessoa);
+		return ResponseEntity.ok(pessoaSalva);
+	}
+	
 }
---
+++ src/main/java/br/com/eskinfotechweb/algamoneyapi/service/PessoaService.java
@@ -0,0 +1,26 @@
+package br.com.eskinfotechweb.algamoneyapi.service;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.stereotype.Service;
+
+import br.com.eskinfotechweb.algamoneyapi.model.Pessoa;
+import br.com.eskinfotechweb.algamoneyapi.repository.PessoaRepository;
+
+@Service
+public class PessoaService {
+
+	@Autowired
+	private PessoaRepository pessoaRepository;
+	
+	public Pessoa Atualizar(Long codigo, Pessoa pessoa) {
+		Pessoa pessoaSalva = pessoaRepository.findOne(codigo);
+		if (pessoaSalva == null) {
+			throw new EmptyResultDataAccessException(1);
+		}
+		BeanUtils.copyProperties(pessoa, pessoaSalva, "codigo");
+		return pessoaRepository.save(pessoaSalva);
+	}
+	
+}
