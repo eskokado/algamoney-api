6d3474cfab75017ef3dfa5b0fea90b05212b0dc6
Edson Shideki Kokado <eskokado@gmail.com>
2019-06-26 09:01:43 -0300

    Implementando a paginação de lançamento

--- src/main/java/br/com/eskinfotechweb/algamoneyapi/repository/lancamento/LancamentoRepositoryQuery.java
+++ src/main/java/br/com/eskinfotechweb/algamoneyapi/repository/lancamento/LancamentoRepositoryQuery.java
@@ -1,12 +1,13 @@
 package br.com.eskinfotechweb.algamoneyapi.repository.lancamento;
 
-import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import br.com.eskinfotechweb.algamoneyapi.model.Lancamento;
 import br.com.eskinfotechweb.algamoneyapi.repository.filter.LancamentoFilter;
 
 public interface LancamentoRepositoryQuery {
 
-	public List<Lancamento> filtrar(LancamentoFilter lancamentoFilter);
+	public Page<Lancamento> filtrar(LancamentoFilter lancamentoFilter, Pageable pageable);
 	
 }
