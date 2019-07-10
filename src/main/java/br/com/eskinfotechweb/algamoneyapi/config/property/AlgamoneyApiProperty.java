245a59c5cefa45539b4efb3495c8c663b1503700
Edson Shideki Kokado <eskokado@gmail.com>
2019-07-10 10:57:22 -0300

    Configurando o envio de e-mail

--- src/main/java/br/com/eskinfotechweb/algamoneyapi/config/property/AlgamoneyApiProperty.java
+++ src/main/java/br/com/eskinfotechweb/algamoneyapi/config/property/AlgamoneyApiProperty.java
@@ -1,24 +1,28 @@
 package br.com.eskinfotechweb.algamoneyapi.config.property;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties("algamoney")
 public class AlgamoneyApiProperty {
-	
-	@Value("${algamoney.origin-permitida}")
+
 	private String originPermitida;
 
 	private final Seguranca seguranca = new Seguranca();
+
+	private final Mail mail = new Mail();
+	
+	public Mail getMail() {
+		return mail;
+	}
 	
 	public String getOriginPermitida() {
 		return originPermitida;
 	}
-	
+
 	public Seguranca getSeguranca() {
 		return seguranca;
 	}
-	
+
 	public static class Seguranca {
 		private boolean enableHttps;
 
@@ -29,8 +33,48 @@ public class AlgamoneyApiProperty {
 		public void setEnableHttps(boolean enableHttps) {
 			this.enableHttps = enableHttps;
 		}
-		
-		
 	}
-	
+
+	public static class Mail {
+		private String host;
+
+		private Integer port;
+
+		private String username;
+
+		private String password;
+
+		public String getHost() {
+			return host;
+		}
+
+		public void setHost(String host) {
+			this.host = host;
+		}
+
+		public Integer getPort() {
+			return port;
+		}
+
+		public void setPort(Integer port) {
+			this.port = port;
+		}
+
+		public String getUsername() {
+			return username;
+		}
+
+		public void setUsername(String username) {
+			this.username = username;
+		}
+
+		public String getPassword() {
+			return password;
+		}
+
+		public void setPassword(String password) {
+			this.password = password;
+		}
+	}
+
 }
